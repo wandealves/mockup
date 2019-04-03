1d43acc966e3576055114edd7887f23b179d8c38
wanderson <wanderson.alves.rodrigues@gmail.com>
2019-04-03 16:02:13 -0300

    Criação

---
+++ material-dashboard/src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+declare const $: any;
+declare interface RouteInfo {
+    path: string;
+    title: string;
+    icon: string;
+    class: string;
+}
+export const ROUTES: RouteInfo[] = [
+    { path: '/dashboard', title: 'Dashboard',  icon: 'dashboard', class: '' },
+    { path: '/user-profile', title: 'Orçamento',  icon:'content_paste', class: '' },
+    { path: '/table-list', title: 'Usuários',  icon:'person', class: '' },
+    { path: '/maps', title: 'Oficinas',  icon:'location_on', class: '' },
+    { path: '/notifications', title: 'Notifications',  icon:'notifications', class: '' },
+];
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent implements OnInit {
+  menuItems: any[];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.menuItems = ROUTES.filter(menuItem => menuItem);
+  }
+  isMobileMenu() {
+      if ($(window).width() > 991) {
+          return false;
+      }
+      return true;
+  };
+}
